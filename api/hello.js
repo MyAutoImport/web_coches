43df0e9709fd8707f6590ee3745ac4a97aea013f
Edgar7222 <1745554@uab.cat>
2025-08-27 19:11:42 +0200

    Fixing errors with the leads

---
+++ api/hello.js
@@ -0,0 +1,5 @@
+// api/hello.js
+export default function handler(req, res) {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.status(200).send('hello from vercel functions');
+}
