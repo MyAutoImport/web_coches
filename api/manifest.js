d06cb9721909fea35170287ff6b241974598af97
Edgar7222 <1745554@uab.cat>
2025-08-30 20:51:35 +0200

    Fixing errors

--- api/manifest.js
+++ api/manifest.js
@@ -1,4 +1,6 @@
 export default function handler(req, res) {
+  const origin = 'https://myautoimport.es'; // fijo y absoluto
+
   const manifest = {
     name: "My Auto Import",
     short_name: "MAI",
@@ -8,8 +10,8 @@ export default function handler(req, res) {
     background_color: "#0b1220",
     theme_color: "#0b1220",
     icons: [
-      { src: "/img/icon-192.png", sizes: "192x192", type: "image/png" },
-      { src: "/img/icon-512.png", sizes: "512x512", type: "image/png" }
+      { src: `${origin}/img/icon-192.png`, sizes: "192x192", type: "image/png", purpose: "any maskable" },
+      { src: `${origin}/img/icon-512.png`, sizes: "512x512", type: "image/png", purpose: "any maskable" }
     ]
   };
 
